0c65804289852e94c32deb6fc22da46ce96b85d5
Ragib Barket <ragibrahman86317@gmail.com>
2024-12-21 18:50:42 +0600

    connect mongodb

--- src/dbConfig/dbConfig.ts
+++ src/dbConfig/dbConfig.ts
@@ -1,1 +1,19 @@
 import mongoose from 'mongoose'
+
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URL!}/nextjs`)
+    const connection = mongoose.connection
+    connection.on('connected', () => {
+      console.log('MongoDB connected successfully')
+    })
+    connection.on('error', (err) => {
+      console.log('MongoDB connection error')
+      console.log(err)
+      process.exit()
+    })
+  } catch (error) {
+    console.log('Database connection failed')
+    console.log(error)
+  }
+}
